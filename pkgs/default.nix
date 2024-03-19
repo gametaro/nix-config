4992caf3f9e106c7816916fe96e51b12612e21c6
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2024-03-20 01:54:00 +0900

    chore: styling

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 pkgs: {
-  goneovim = pkgs.callPackage ./goneovim.nix { inherit pkgs;};
+  goneovim = pkgs.callPackage ./goneovim.nix {inherit pkgs;};
 }
